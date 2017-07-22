71c52aaa1014aab5f993649b6f4d8582f1f385be
Vishal Rana <vr@labstack.com>
2017-07-21 21:17:15 -0700

    Bumped v0.3.1
    Signed-off-by: Vishal Rana <vr@labstack.com>

--- setup.py
+++ setup.py
@@ -2,13 +2,15 @@ from setuptools import setup
 
 setup(
   name='labstack',
-  version='0.3.0',
+  version='0.3.1',
   description='Official Python client library for the LabStack REST API',
   long_description='`<https://github.com/labstack/labstack-python>`_',
   keywords='labstack cube email log mqtt store',
+  url='https://github.com/labstack/labstack-python',
   author='Vishal Rana',
   author_email='vr@labstack.com',
   license='MIT',
+  packages=['labstack'],
   install_requires=[
     'requests==2.18.1',
     'arrow==0.10.0'
