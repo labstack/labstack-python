529c55e5c734f1101419112c9161875fb2312216
Vishal Rana <vr@labstack.com>
2017-08-31 11:34:34 -0700

    Bumped v0.6.1
    Signed-off-by: Vishal Rana <vr@labstack.com>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
   name='labstack',
-  version='0.6.0',
+  version='0.6.1',
   description='Official Python client library for the LabStack platform',
   long_description='`<https://github.com/labstack/labstack-python>`_',
   keywords='labstack cube email log mqtt store',
