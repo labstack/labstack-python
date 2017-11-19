af4f06bf27797b799e41dce9670242329c1bad33
Vishal Rana <vr@labstack.com>
2017-11-19 13:49:54 -0800

    Per new api
    Signed-off-by: Vishal Rana <vr@labstack.com>

--- labstack/client.py
+++ labstack/client.py
@@ -1,6 +1,6 @@
 import requests
-from .hub import _Hub
-from .jet import _Jet
+
+API_URL = 'https://api.labstack.com'
 
 class _Interceptor(requests.auth.AuthBase):
   def __init__(self, api_key):
@@ -8,18 +8,39 @@ class _Interceptor(requests.auth.AuthBase):
 
   def __call__(self, r):
     r.headers['Authorization'] = 'Bearer ' + self.api_key
-    r.headers['Content-Type'] = 'application/json; charset=utf-8'
     return r
 
 class Client():
-  def __init__(self, account_id, api_key):
-    self.account_id = account_id
+  def __init__(self, api_key):
     self.api_key = api_key
     self.interceptor = _Interceptor(api_key)
 
-  def hub(self, client_id):
-    return _Hub(self.account_id, self.api_key, client_id)
+  def image_compress(self, file=None):
+    files = {'file': open(file, 'rb')}
+    r = requests.post('{}/image/compress'.format(API_URL), auth=self.interceptor, files=files)
+    data = r.json()
+    if not 200 <= r.status_code < 300:
+      raise APIError(data['code'], data['message'])
+    return data
 
-  def jet(self):
-    return _Jet(self.interceptor)
+  def image_resize(self, file=None, width=None, height=None, crop=None):
+    files = {'file': open(file, 'rb')}
+    data = {
+      'width': width,
+      'height': height,
+      'crop': crop
+    }
+    r = requests.post('{}/image/resize'.format(API_URL), auth=self.interceptor, 
+      files=files, data=data)
+    data = r.json()
+    if not 200 <= r.status_code < 300:
+      raise APIError(data['code'], data['message'])
+    return data  
+
+class APIError(Exception):
+  def __init__(self, code, message):
+    self.code = code
+    self.message = message
   
+  def __str__(self):
+    return self.messag
