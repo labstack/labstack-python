78e96fe8dd74048a634e5ea2c8b4d96458cc456a
Vishal Rana <vr@labstack.com>
2018-02-28 22:32:31 -0800

    Restructured
    Signed-off-by: Vishal Rana <vr@labstack.com>

--- labstack/client.py
+++ labstack/client.py
@@ -26,118 +26,107 @@ class Client():
           f.write(chunk)
           f.flush()
 
-  def barcode_generate(self, format=None, content=None, size=None):
-    json = {
-      'format': format,
-      'content': content,
-      'size': size
+  def currency_convert(self, from=None, to=None, value=None):
+    params = {
+      'from': from,
+      'to': to,
+      'value': value
     }
-    r = requests.post(API_URL + '/barcode/generate', auth=self.interceptor,
-      json=json)
+    r = requests.get(API_URL + '/currency/convert', auth=self.interceptor,
+      params=params)
+    data = r.json()
+    if self._error(r):
+      raise APIError(data['code'], data['message'])
+    return data
+
+  def currency_rates(self, base=None):
+        params = {'base': base}
+    r = requests.get(API_URL + '/currency/rates', auth=self.interceptor,
+      params=params)
     data = r.json()
     if self._error(r):
       raise APIError(data['code'], data['message'])
     return data
   
-  def barcode_scan(self, file=None):
-    files = {'file': open(file, 'rb')}
-    r = requests.post(API_URL + '/barcode/scan', auth=self.interceptor, files=files)
-    data = r.json()
-    if self._error(r):
-      raise APIError(data['code'], data['message'])
-    return data
-
-  def currency_convert(self, base=None):
-    json = {'base': base}
-    r = requests.post(API_URL + '/currency/convert', auth=self.interceptor,
-      json=json)
-    data = r.json()
-    if self._error(r):
-      raise APIError(data['code'], data['message'])
-    return data
-
-  
-  def dns_lookup(self, domain=None, type=None):
-    json = {
-      'domain': domain,
-      'type': type
-    }
-    r = requests.post(API_URL + '/dns/lookup', auth=self.interceptor,
-      json=json)
-    data = r.json()
-    if self._error(r):
-      raise APIError(data['code'], data['message'])
-    return data
-
   def email_verify(self, email=None):
-    json = {'email': email}
-    r = requests.post(API_URL + '/email/verify', auth=self.interceptor,
-      json=json)
+    params = {'email': email}
+    r = requests.get(API_URL + '/email/verify', auth=self.interceptor,
+      params=params)
     data = r.json()
     if self._error(r):
       raise APIError(data['code'], data['message'])
     return data
   
   def geocode_address(self, location=None, longitude=None, latitude=None, osm_tag=None, limit=None):
-    json = {
+    params = {
       'location': location,
       'longitude': longitude,
       'latitude': latitude,
       'osm_tag': osm_tag,
       'limit': limit
     }
-    r = requests.post(API_URL + '/geocode/address', auth=self.interceptor,
-      json=json)
+    r = requests.get(API_URL + '/geocode/address', auth=self.interceptor,
+      params=params)
     data = r.json()
     if self._error(r):
       raise APIError(data['code'], data['message'])
     return data
   
   def geocode_ip(self, ip=None):
-    json = {'ip': ip}
-    r = requests.post(API_URL + '/geocode/ip', auth=self.interceptor,
-      json=json)
+    params = {'ip': ip}
+    r = requests.get(API_URL + '/geocode/ip', auth=self.interceptor,
+      params=params)
     data = r.json()
     if self._error(r):
       raise APIError(data['code'], data['message'])
     return data
   
   def geocode_reverse(self, longitude=None, latitude=None, limit=None):
-    json = {
+    params = {
       'longitude': longitude,
       'latitude': latitude,
       'limit': limit
     }
-    r = requests.post(API_URL + '/geocode/reverse', auth=self.interceptor,
-      json=json)
+    r = requests.get(API_URL + '/geocode/reverse', auth=self.interceptor,
+      params=params)
+    data = r.json()
+    if self._error(r):
+      raise APIError(data['code'], data['message'])
+    return data
+  
+  def compress_audio(self, file=None):
+        files = {'file': open(file, 'rb')}
+    r = requests.post(API_URL + '/compress/audio', auth=self.interceptor, files=files)
     data = r.json()
     if self._error(r):
       raise APIError(data['code'], data['message'])
     return data
 
-  def image_compress(self, file=None):
+  def compress_image(self, file=None):
     files = {'file': open(file, 'rb')}
-    r = requests.post(API_URL + '/image/compress', auth=self.interceptor, files=files)
+    r = requests.post(API_URL + '/compress/image', auth=self.interceptor, files=files)
+    data = r.json()
+    if self._error(r):
+      raise APIError(data['code'], data['message'])
+    return data
+  
+  def compress_pdf(self, file=None):
+        files = {'file': open(file, 'rb')}
+    r = requests.post(API_URL + '/compress/pdf', auth=self.interceptor, files=files)
+    data = r.json()
+    if self._error(r):
+      raise APIError(data['code'], data['message'])
+    return data
+  
+  def compress_video(self, file=None):
+        files = {'file': open(file, 'rb')}
+    r = requests.post(API_URL + '/compress/video', auth=self.interceptor, files=files)
     data = r.json()
     if self._error(r):
       raise APIError(data['code'], data['message'])
     return data
 
-  def image_resize(self, file=None, width=None, height=None, format=None):
-    files = {'file': open(file, 'rb')}
-    data = {
-      'width': width,
-      'height': height,
-      'format': format
-    }
-    r = requests.post('{}/image/resize'.format(API_URL), auth=self.interceptor, 
-      files=files, data=data)
-    data = r.json()
-    if self._error(r):
-      raise APIError(data['code'], data['message'])
-    return data
-
-  def image_watermark(self, file=None, text=None, font=None, size=None, color=None, opacity=None,
+  def watermark_image(self, file=None, text=None, font=None, size=None, color=None, opacity=None,
     position=None, margin=None):
     files = {'file': open(file, 'rb')}
     data = {
@@ -149,44 +138,14 @@ class Client():
       'position': position,
       'margin': margin
     }
-    r = requests.post('{}/image/watermark'.format(API_URL), auth=self.interceptor, 
+    r = requests.post('{}/watermark/image'.format(API_URL), auth=self.interceptor, 
       files=files, data=data)
     data = r.json()
     if self._error(r):
       raise APIError(data['code'], data['message'])
     return data  
-
-  def pdf_compress(self, file=None):
-    files = {'file': open(file, 'rb')}
-    r = requests.post(API_URL + '/pdf/compress', auth=self.interceptor, files=files)
-    data = r.json()
-    if self._error(r):
-      raise APIError(data['code'], data['message'])
-    return data
   
-  def pdf_image(self, file=None, extract=None):
-    files = {'file': open(file, 'rb')}
-    data = {
-      'extract': extract
-    }
-    r = requests.post(API_URL + '/pdf/image', auth=self.interceptor, files=files, data=data)
-    data = r.json()
-    if self._error(r):
-      raise APIError(data['code'], data['message'])
-    return data
-
-  def pdf_split(self, file=None, pages=None):
-    files = {'file': open(file, 'rb')}
-    data = {
-      'pages': pages 
-    }
-    r = requests.post(API_URL + '/pdf/split', auth=self.interceptor, files=files, data=data)
-    data = r.json()
-    if self._error(r):
-      raise APIError(data['code'], data['message'])
-    return data
-  
-  def text_sentiment(self, text=None):
+  def nlp_sentiment(self, text=None):
     json = {'text': text}
     r = requests.post(API_URL + '/text/sentiment', auth=self.interceptor,
       json=json)
@@ -195,7 +154,7 @@ class Client():
       raise APIError(data['code'], data['message'])
     return data
   
-  def text_spellcheck(self, text=None):
+  def nlp_spellcheck(self, text=None):
     json = {'text': text}
     r = requests.post(API_URL + '/text/spellcheck', auth=self.interceptor,
       json=json)
@@ -204,7 +163,7 @@ class Client():
       raise APIError(data['code'], data['message'])
     return data
   
-  def text_summary(self, text=None, url=None, language=None, length=None):
+  def nlp_summary(self, text=None, url=None, language=None, length=None):
     json = {
       'text': text,
       'url': url,
@@ -218,23 +177,14 @@ class Client():
       raise APIError(data['code'], data['message'])
     return data
   
-  def webpage_pdf(self, url=None, size=None, layout=None):
-    json = {
+  def webpage_pdf(self, url=None, layout=None, format=None):
+    params = {
       'url': url,
-      'size': size,
       'layout': layout
+      'format': format,
     }
-    r = requests.post(API_URL + '/webpage/pdf', auth=self.interceptor,
-      json=json)
-    data = r.json()
-    if self._error(r):
-      raise APIError(data['code'], data['message'])
-    return data
-
-  def word_lookup(self, word=None):
-    json = {'word': word}
-    r = requests.post(API_URL + '/word/lookup', auth=self.interceptor,
-      json=json)
+    r = requests.get(API_URL + '/webpage/pdf', auth=self.interceptor,
+      params=params)
     data = r.json()
     if self._error(r):
       raise APIError(data['code'], data['message'])
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
   name='labstack',
-  version='0.20.0',
+  version='0.21.0',
   description='Official Python client library for the LabStack API',
   long_description='`<https://github.com/labstack/labstack-python>`_',
   keywords='image compress, image resize, text summary, barcode generate, barcode scan',
